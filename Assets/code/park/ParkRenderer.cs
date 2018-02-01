58c150bf2fe7516049127320c54a724ae363109e
Cody Swendrowski <cody@swendrowski.us>
2018-01-31 21:22:31 -0600

    Fixes project structure

---
+++ Assets/code/park/ParkRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkRenderer : MonoBehaviour {
+
+	private int worldHeight = 10;
+	private int worldWidth = 10;
+
+	[SerializeField] GameObject basicTile;
+
+	private GameObject[,] parkTiles;
+
+	// Use this for initialization
+	void Start () {
+		parkTiles = new GameObject[worldWidth,worldHeight];
+
+		for (int x = 0; x < worldWidth; x++) {
+			for (int y = 0; y < worldHeight; y++) {
+				parkTiles[x,y] = Instantiate (basicTile, new Vector3 (x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+
+}
