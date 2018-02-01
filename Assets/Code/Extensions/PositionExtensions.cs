b2bc267ccbffc8f787202f969ac0cd3aa74a7dbd
Cody Swendrowski <cody@swendrowski.us>
2018-01-31 22:25:40 -0600

    Adds helper extensions for Position to Vector3

---
+++ Assets/Code/Extensions/PositionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Code.Models;
+using UnityEngine;
+
+namespace Assets.Code.Extensions
+{
+    public static class PositionExtensions
+    {
+        public static Vector3 ToVector3(this Position position)
+        {
+            return new Vector3(position.X, position.Y);
+        }
+    }
+}
