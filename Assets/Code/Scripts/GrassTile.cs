71d90b80e2f4e11a28ca422a5c3bbc5cd45ff80b
johngillett <jgillett@pugetsound.edu>
2018-02-28 21:48:21 -0700

    (Very) Basic Tile Interaction
    Click on Grass Tiles to say Yo

---
+++ Assets/Code/Scripts/GrassTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Code.Scripts;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GrassTile : RandomTile, InteractableTile {
+
+	public void interact (ToolBar.ToolType tool) {
+		Debug.Log ("YO WHATS UP IM A GRASS TILE");
+	}
+
+	#if UNITY_EDITOR
+	[MenuItem("Assets/Create/Grass Tile")]
+	public static void CreateGrassTile()
+	{
+		string path = EditorUtility.SaveFilePanelInProject("Save Grass Tile", "New Grass Tile", "asset", "Save Grass Tile", "Assets");
+		if (path == "")
+			return;
+
+		AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<GrassTile>(), path);
+	}
+	#endif
+
+}
+
+
